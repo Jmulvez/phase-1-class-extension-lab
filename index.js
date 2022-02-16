99c52f4139cf877545484f8d8a1b3e7960761180
John <johnmulvey48@yahoo.com>
2022-02-16 10:38:30 -0500

    git@github.com:Jmulvez/phase-1-class-extension-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+class polygon {
+    constructor(integers) {
+        this.integers = integers;
+    }
+    get countSides(sides) {
+        return sides;
+    }
+    get perimeter(perimeter) {
+        const sum = array.reduce(function(sides)) {
+            return sum
+        }
+    }
+}
+
+class triangle extends polygon {
+    constructor(...arg) {
+        get isValid() {
+            return true
+        }
+    }
+}
+
+class square extends polygon {
+    constructor(...arg) {
+        get isValid() {
+            return true
+        }
+    }
+}
